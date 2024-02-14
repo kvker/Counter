0e37567bdabe2b05b9a7297f4b4c105180fabfa6
leghair <zweizhao@foxmail.com>
2024-02-15 00:34:12 +0800

    feat 历史记录页面完成

--- pages/history/history.vue
+++ pages/history/history.vue
@@ -9,14 +9,56 @@
     title: '万能计数器',
     path: '/pages/index/index'
   }))
+
+  const mainData : Ref<MainData> = ref(uni.getStorageSync('mainData'))
+
+
+  console.log(mainData)
 </script>
 
 <template>
-  <view class="page">
-
+  <view class="page flex">
+    <view class="game w-100 mb-20" v-for="(game, gameIndex) of mainData.games" :key="'game' + gameIndex">
+      <view class="game-name">{{game.name}}</view>
+      <view class="sub-game" v-for="(subGame, subGameIndex) of game.children" :key="'subGame' + subGameIndex">
+        <view class="sub-game-name">{{subGame.name}}</view>
+        <view class="player flex aic jcsb" v-for="(player, playerIndex) of subGame.players" :key="'player' + playerIndex">
+          <view class="player-name">{{player.name}}</view>
+          <view class="player-score">{{player.score}}</view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <style scoped>
+  .page {
+    height: auto;
+    min-height: auto;
+    padding: 10vw 5vw;
+    flex-direction: column-reverse;
+  }
 
+  .game {
+    border: 2rpx solid white;
+    border-radius: 16rpx;
+    padding: 16rpx;
+    color: white;
+  }
+
+  .game-name {
+    font-size: 48rpx;
+  }
+  
+  .sub-game-name {
+    font-size: 40rpx;
+  }
+  
+  .player-name {
+    font-size: 32rpx;
+  }
+  
+  .player-score {
+    font-size: 32rpx;
+  }
 </style>
